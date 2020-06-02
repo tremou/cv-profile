695808c9daa2b5be668dadb9194da5f9b29fbb0b
Olivier Girardot <olive_girardot@hotmail.com>
2020-06-02 12:28:29 +0200

    doing some general testing with new features

--- javascript.js
+++ javascript.js
@@ -3,19 +3,6 @@
     event.currentTarget.classList.toggle("img-circle");
   });
 }); */
+console.log("Hello World");
 
-
-var cardElement = document.getElementById("test");
-var linkAppear = document.getElementById("test");
-
-function myFunction() {
-  linkAppear.style.visibility = "hidden";
-}
-
-function myOtherFunction() {
-  linkAppear.style.visibility = "visible";
-}
-
-cardElement.addEventListener("mouseover", myFunction);
-cardElement.addEventListener("mouseleave", myOtherFunction);
-
+const addButton = document.querySelector(".lin-kcard");

71add3244b44348b72e68ba4bd67f762413a7224
Olivier Girardot <olive_girardot@hotmail.com>
2020-06-06 21:51:32 +0200

    made the navbar responsive with javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,36 @@
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu");
 
+console.log(navbar);
+console.log(menu);
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 700) {
+    navbar.classList.remove('navbar');
+    navbar.classList.add("navbar-under");
+  }
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY < 700) {
+    navbar.classList.remove("navbar-under");
+    navbar.classList.add('navbar');
+  }
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 700) {
+    menu.classList.remove('menu');
+    menu.classList.add("menu-under");
+  }
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY < 700) {
+    menu.classList.remove("menu-under");
+    menu.classList.add('menu');
+  }
+});
+
+console.log(navbar);
+console.log(menu);

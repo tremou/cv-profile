33d9d405a3a26692f832d751af08a5d63b75c01f
Olivier Girardot <olive_girardot@hotmail.com>
2020-06-10 18:15:21 +0200

    adjusted navbar change when scroll down to match hd screen

--- javascript.js
+++ javascript.js
@@ -2,28 +2,28 @@ const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 700) {
+  if (window.scrollY > 1040) {
     navbar.classList.remove('navbar');
     navbar.classList.add("navbar-under");
   }
 });
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY < 700) {
+  if (window.scrollY < 1040) {
     navbar.classList.remove("navbar-under");
     navbar.classList.add('navbar');
   }
 });
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 700) {
+  if (window.scrollY > 1040) {
     menu.classList.remove('menu');
     menu.classList.add("menu-under");
   }
 });
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY < 700) {
+  if (window.scrollY < 1040) {
     menu.classList.remove("menu-under");
     menu.classList.add('menu');
   }

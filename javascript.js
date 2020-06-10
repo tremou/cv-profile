2a3737d9a6092955d0a92e8a9f80ffe605caae5d
Olivier Girardot <olive_girardot@hotmail.com>
2020-06-10 18:53:00 +0200

    adjusted the navbar scroll change to more devices

--- javascript.js
+++ javascript.js
@@ -1,30 +1,128 @@
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
 
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 1040) {
+  if (window.scrollY > 730) {
     navbar.classList.remove('navbar');
     navbar.classList.add("navbar-under");
   }
 });
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY < 1040) {
+  if (window.scrollY < 730) {
     navbar.classList.remove("navbar-under");
     navbar.classList.add('navbar');
   }
 });
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 1040) {
+  if (window.scrollY > 730) {
     menu.classList.remove('menu');
     menu.classList.add("menu-under");
   }
 });
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY < 1040) {
+  if (window.scrollY < 730) {
     menu.classList.remove("menu-under");
     menu.classList.add('menu');
   }
 });
+
+console.log(vw);
+console.log(vh);
+
+if (vw < 1300) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 630) {
+      navbar.classList.remove('navbar');
+      navbar.classList.add("navbar-under");
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY < 630) {
+      navbar.classList.remove("navbar-under");
+      navbar.classList.add('navbar');
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 630) {
+      menu.classList.remove('menu');
+      menu.classList.add("menu-under");
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY < 630) {
+      menu.classList.remove("menu-under");
+      menu.classList.add('menu');
+    }
+  });
+}
+
+
+if (vw < 1150) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 470) {
+      navbar.classList.remove('navbar');
+      navbar.classList.add("navbar-under");
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY < 470) {
+      navbar.classList.remove("navbar-under");
+      navbar.classList.add('navbar');
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 470) {
+      menu.classList.remove('menu');
+      menu.classList.add("menu-under");
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY < 470) {
+      menu.classList.remove("menu-under");
+      menu.classList.add('menu');
+    }
+  });
+}
+
+if (vw < 900) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 390) {
+      navbar.classList.remove('navbar');
+      navbar.classList.add("navbar-under");
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY < 390) {
+      navbar.classList.remove("navbar-under");
+      navbar.classList.add('navbar');
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 390) {
+      menu.classList.remove('menu');
+      menu.classList.add("menu-under");
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY < 390) {
+      menu.classList.remove("menu-under");
+      menu.classList.add('menu');
+    }
+  });
+}

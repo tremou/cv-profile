a3b28d990de4de04bf8c0dade331995590777af4
Olivier Girardot <olive_girardot@hotmail.com>
2020-06-15 19:52:06 +0200

    replaced addEventListener scroll with Intersection observer --- Awesome

--- javascript.js
+++ javascript.js
@@ -1,139 +1,148 @@
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
 
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+// const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+// const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 730) {
-    navbar.classList.remove('navbar');
-    navbar.classList.add("navbar-under");
-  }
-});
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > 730) {
+//     navbar.classList.remove('navbar');
+//     navbar.classList.add("navbar-under");
+//   }
+// });
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY < 730) {
-    navbar.classList.remove("navbar-under");
-    navbar.classList.add('navbar');
-  }
-});
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY < 730) {
+//     navbar.classList.remove("navbar-under");
+//     navbar.classList.add('navbar');
+//   }
+// });
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 730) {
-    menu.classList.remove('menu');
-    menu.classList.add("menu-under");
-  }
-});
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > 730) {
+//     menu.classList.remove('menu');
+//     menu.classList.add("menu-under");
+//   }
+// });
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY < 730) {
-    menu.classList.remove("menu-under");
-    menu.classList.add('menu');
-  }
-});
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY < 730) {
+//     menu.classList.remove("menu-under");
+//     menu.classList.add('menu');
+//   }
+// });
 
-if (vw < 1300) {
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 630) {
-      navbar.classList.remove('navbar');
-      navbar.classList.add("navbar-under");
-    }
-  });
+// if (vw < 1300) {
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 630) {
+//       navbar.classList.remove('navbar');
+//       navbar.classList.add("navbar-under");
+//     }
+//   });
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < 630) {
-      navbar.classList.remove("navbar-under");
-      navbar.classList.add('navbar');
-    }
-  });
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY < 630) {
+//       navbar.classList.remove("navbar-under");
+//       navbar.classList.add('navbar');
+//     }
+//   });
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 630) {
-      menu.classList.remove('menu');
-      menu.classList.add("menu-under");
-    }
-  });
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 630) {
+//       menu.classList.remove('menu');
+//       menu.classList.add("menu-under");
+//     }
+//   });
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < 630) {
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY < 630) {
+//       menu.classList.remove("menu-under");
+//       menu.classList.add('menu');
+//     }
+//   });
+// }
+
+
+// if (vw < 1150) {
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 470) {
+//       navbar.classList.remove('navbar');
+//       navbar.classList.add("navbar-under");
+//     }
+//   });
+
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY < 470) {
+//       navbar.classList.remove("navbar-under");
+//       navbar.classList.add('navbar');
+//     }
+//   });
+
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 470) {
+//       menu.classList.remove('menu');
+//       menu.classList.add("menu-under");
+//     }
+//   });
+
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY < 470) {
+//       menu.classList.remove("menu-under");
+//       menu.classList.add('menu');
+//     }
+//   });
+// }
+
+// if (vw < 900) {
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 390) {
+//       navbar.classList.remove('navbar');
+//       navbar.classList.add("navbar-under");
+//     }
+//   });
+
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY < 390) {
+//       navbar.classList.remove("navbar-under");
+//       navbar.classList.add('navbar');
+//     }
+//   });
+
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 390) {
+//       menu.classList.remove('menu');
+//       menu.classList.add("menu-under");
+//     }
+//   });
+
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY < 390) {
+//       menu.classList.remove("menu-under");
+//       menu.classList.add('menu');
+//     }
+//   });
+// }
+
+
+
+const target = document.querySelector('.banner');
+
+const options = {};
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting === false) {
+      menu.classList.remove("menu");
+      menu.classList.add('menu-under');
+    } else {
       menu.classList.remove("menu-under");
       menu.classList.add('menu');
     }
   });
-}
+};
 
+const observer = new IntersectionObserver(callback, options);
 
-if (vw < 1150) {
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 470) {
-      navbar.classList.remove('navbar');
-      navbar.classList.add("navbar-under");
-    }
-  });
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < 470) {
-      navbar.classList.remove("navbar-under");
-      navbar.classList.add('navbar');
-    }
-  });
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 470) {
-      menu.classList.remove('menu');
-      menu.classList.add("menu-under");
-    }
-  });
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < 470) {
-      menu.classList.remove("menu-under");
-      menu.classList.add('menu');
-    }
-  });
-}
-
-if (vw < 900) {
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 390) {
-      navbar.classList.remove('navbar');
-      navbar.classList.add("navbar-under");
-    }
-  });
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < 390) {
-      navbar.classList.remove("navbar-under");
-      navbar.classList.add('navbar');
-    }
-  });
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 390) {
-      menu.classList.remove('menu');
-      menu.classList.add("menu-under");
-    }
-  });
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < 390) {
-      menu.classList.remove("menu-under");
-      menu.classList.add('menu');
-    }
-  });
-}
-
-
-const test = () => { console.log('hello') };
-
-let options = {
-  root: document.querySelector('.banner'),
-  rootMargin: '0px',
-  threshold: 1.0
-}
-
-let observer = new IntersectionObserver(test, options);
-
-let target = document.querySelector('.menu-iphone');
 observer.observe(target);
+
